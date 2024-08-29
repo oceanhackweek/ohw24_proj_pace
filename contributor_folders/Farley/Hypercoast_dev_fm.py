6abb53112d8efafced14ca6412be10afa6164e6c
farleyw <59060341+farleyw@users.noreply.github.com>
2024-08-29 13:16:39 -0400

    Hypercoast Datacube

--- contributor_folders/Farley/Hypercoast_dev_fm.py
+++ contributor_folders/Farley/Hypercoast_dev_fm.py
@@ -5,3 +5,78 @@ Created on Thu Aug 29 09:03:29 2024
 @author: fmiller
 """
 #Here we will experiment with hypercoast
+
+import os
+
+from holoviews.streams import Tap
+from matplotlib import animation
+from matplotlib.colors import ListedColormap
+from PIL import Image, ImageEnhance
+from scipy.ndimage import gaussian_filter1d
+#from xarray.backends.api import open_datatree
+import cartopy.crs as ccrs
+import cmocean
+import earthaccess
+import holoviews as hv
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pl
+import numpy as np
+import panel.widgets as pnw
+import xarray as xr
+hv.extension("bokeh")
+import hypercoast
+import colour
+import colormath
+from colormath.color_objects import SpectralColor
+from colormath.color_conversions import convert_color
+
+#Load in example data
+#TODO make it work with our data!
+#Import dataset from EarthAcess
+
+def wavelength_to_rgb(wavelength, gamma=0.8):
+
+    '''This converts a given wavelength of light to an 
+    approximate RGB color value. The wavelength must be given
+    in nanometers in the range from 380 nm through 750 nm
+    (789 THz through 400 THz).
+    Based on code by Dan Bruton
+    http://www.physics.sfasu.edu/astro/color/spectra.html
+    '''
+
+    wavelength = float(wavelength)
+    if wavelength >= 380 and wavelength <= 440:
+        attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
+        R = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
+        G = 0.0
+        B = (1.0 * attenuation) ** gamma
+    elif wavelength >= 440 and wavelength <= 490:
+        R = 0.0
+        G = ((wavelength - 440) / (490 - 440)) ** gamma
+        B = 1.0
+    elif wavelength >= 490 and wavelength <= 510:
+        R = 0.0
+        G = 1.0
+        B = (-(wavelength - 510) / (510 - 490)) ** gamma
+    elif wavelength >= 510 and wavelength <= 580:
+        R = ((wavelength - 510) / (580 - 510)) ** gamma
+        G = 1.0
+        B = 0.0
+    elif wavelength >= 580 and wavelength <= 645:
+        R = 1.0
+        G = (-(wavelength - 645) / (645 - 580)) ** gamma
+        B = 0.0
+    elif wavelength >= 645 and wavelength <= 750:
+        attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
+        R = (1.0 * attenuation) ** gamma
+        G = 0.0
+        B = 0.0
+    else:
+        R = 0.0
+        G = 0.0
+        B = 0.0
+    R *= 255
+    G *= 255
+    B *= 255
+    return (int(R), int(G), int(B))
+
